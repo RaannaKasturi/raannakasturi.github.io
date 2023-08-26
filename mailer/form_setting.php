b5398def64e124e5729a9f5e3459418ec1da62cc
Nayan <raannakasturi@gmail.com>
2023-08-26 20:46:59 +0530

    initial commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Raanna Kasturi'"; //letter subject
+	$to = 'raannakasturi@gmail.com'; // Enter Your E-mail
+	$from = 'admin@raannakasturi.eu.org'; // Admin e-mail
+	$fromName = 'Raanna Kasturi'; // Your company name
+	$charset = 'UTF-8';
+?>
